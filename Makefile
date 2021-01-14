fedacc07b56f6ff3eb792665f52170d5a2c06fe3
none <user@example.com>
2021-01-13 16:59:15 -0800

    Some basic build infrastructure

---
+++ Makefile
@@ -0,0 +1,124 @@
+#
+# Makefile for messing around with the MRS Microplex devices
+#
+
+#
+# Generic actions (targets) that the user may invoke.
+#
+ACTIONS		 = build clean flash
+
+#
+# Locate the root of the source tree.
+#
+export SRCROOT	:= $(dir $(lastword $(MAKEFILE_LIST)))
+
+#
+# Things we will try to build
+#
+APPS	 	 ?= blink
+
+#
+# Build a full list of targets.
+#
+TARGETS		 = $(foreach action,$(ACTIONS), \
+			$(foreach app,$(APPS), \
+				$(action)~$(app)))
+
+all:		build
+
+#
+# Composite targets for handling actions.
+#
+build:		$(filter build~%,$(TARGETS))
+clean:		$(filter clean~%,$(TARGETS))
+flash:		$(filter flash~%,$(TARGETS))
+
+#
+# Call the app makefiles to do the things
+#
+$(TARGETS):	config  = $(subst ~, ,$@)
+$(TARGETS):	action  = $(word 1, $(config))
+$(TARGETS):	app	= $(word 2, $(config))
+$(TARGETS):
+	@echo % $(action) $(app)
+	@make -f $(app)/app.mk $(action) \
+		APP=$(APP)
+
+.PHONY:	$(ACTIONS) $(TARGETS)
+
+help:
+	@echo ""
+	@echo "Build things."
+	@echo ""
+	@echo "Common targets are:"
+	@echo ""
+	@echo "    all         - Builds everything, this is the default target."
+	@echo "    clean       - Cleans all build products"
+	@echo ""
+	@echo "A specific app may be built by selecting one or more of the following targets:"
+	@echo ""
+	@for i in $(TARGETS); do echo "    $$i"; done
+	@echo ""
+	@echo "The following maintenance targets are mostly of interest to"
+	@echo "developers:"
+	@echo ""
+	@echo "    format      - Automatically (re)formats source code to conform"
+	@echo "                  to the project coding style.  Should be used before"
+	@echo "                  checking in or submitting a patch."
+	@echo ""
+
+
+.PHONY:	format
+format:
+	astyle -r $(FMT) "*.c" "*.h"
+#
+# Below are options passed to Artistic Style for the format target
+#
+
+# Basic style is what AS calls K&R
+FMT		+= --style=kr
+
+# Indent using 8-space tabs
+FMT		+= --indent=tab=8
+
+# Indent multiline preprocessor blocks
+FMT		+= --indent-preprocessor
+
+# Do smart multiline expression indentation
+FMT		+= --min-conditional-indent=0
+
+# Pad conditionals and else blocks
+# Not sure about this one yet
+#FMT		+= --break-blocks=all
+
+# Add space padding around operators
+FMT		+= --pad-oper
+
+# Add space padding after paren headers
+FMT		+= --pad-header
+
+# Clean up excess paren padding
+FMT		+= --unpad-paren
+
+# Avoid breaking up 'clever' formatting
+FMT		+= --keep-one-line-blocks
+FMT		+= --keep-one-line-statements
+
+# * belongs with the name (looks good, but syntactically bad)
+FMT		+= --align-pointer=name
+
+# force *nix line endings
+FMT		+= --lineend=linux
+
+# ignore various things that should be ignored
+FMT		+= --ignore-exclude-errors-x
+FMT		+= --exclude=obj
+
+# ignore various files we want to be able to diff with upstream providers
+FMT		+= --exclude=Si1000_defs.h
+FMT		+= --exclude=Si1020_defs.h
+FMT		+= --exclude=compiler_defs.h
+FMT		+= --exclude=rtPhy.c
+FMT		+= --exclude=rtPhy.h
+FMT		+= --exclude=rtPhy_defs.h
+FMT		+= --exclude=printfl.c
---
+++ include/rules.mk
@@ -0,0 +1,129 @@
+#
+# Copyright (c) 2011 Michael Smith, All Rights Reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  o Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer.
+#  o Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# Common rules and targets.
+#
+
+ifeq ($(BOARD),)
+$(error BOARD not defined)
+endif
+ifeq ($(SRCROOT),)
+$(error SRCROOT not defined)
+endif
+
+#
+# Common build options.
+#
+CFLAGS		+=	-DBOARD_$(BOARD) \
+			--std-sdcc11
+
+LDFLAGS		 =	--code-loc 0x2200 \
+			--out-fmt-s19
+
+#
+# Paths.
+#
+OBJROOT		?=	$(SRCROOT)/obj/$(BOARD)/$(PRODUCT)
+DSTROOT		?=	$(SRCROOT)/dst
+LIB_DIR		 =	$(SRCROOT)/lib
+
+#
+# Buildable and installable products
+#
+PRODUCT_SREC	 =	$(OBJROOT)/$(PRODUCT).s19
+PRODUCT_INSTALL	?=	$(PRODUCT_SREC)
+
+#
+# Compiler and tools
+#
+ifeq ($(shell which sdcc),)
+$(error Could not find SDCC on your path - cannot build)
+endif
+CC		 =	sdcc -ms08
+AS		 =	sdas6808 -losp
+LD		 =	sdcc -ms08
+INCLUDES	 =	$(SRCROOT)/include
+CFLAGS		+=	$(addprefix -I,$(INCLUDES))
+DEPFLAGS	 =	-MM $(CFLAGS)
+
+#
+# Assembly source/objects must come first to ensure startup files
+# can be in front.
+#
+ASRCS		+=	$(sort $(wildcard $(PRODUCT_DIR)/*.asm))
+OBJS		+=	$(patsubst $(PRODUCT_DIR)/%.asm,$(OBJROOT)/%.rel,$(ASRCS))
+CSRCS		+=	$(wildcard $(PRODUCT_DIR)/*.c)
+LIBSRCS		+=	$(foreach module,$(LIB_MODULES),$(LIB_DIR)/$(module).c)
+OBJS		+=	$(patsubst $(PRODUCT_DIR)/%.c,$(OBJROOT)/%.rel,$(CSRCS))
+OBJS		+=	$(patsubst $(LIB_DIR)/%.c,$(OBJROOT)/%.rel,$(LIBSRCS))
+#$(info $(OBJS))
+
+ifeq ($(VERBOSE),)
+v		 =	@
+else
+CFLAGS		+=	-V
+endif
+
+#
+# Build rules
+#
+build:	$(PRODUCT_SREC)
+
+$(PRODUCT_SREC):	$(OBJS)
+	@echo LD $@
+	@mkdir -p $(@D)
+	$(v)$(LD) $(LDFLAGS) -o $@ $(OBJS)
+
+$(OBJROOT)/%.rel: $(PRODUCT_DIR)/%.c
+	@mkdir -p $(@D)
+	@echo DEP $<
+	$(v)(/bin/echo -n $(@D)/ && $(CC) $(DEPFLAGS) $<) > $@.dep
+	@echo CC $<
+	$(v)$(CC) -c -o $@ $(CFLAGS) $<
+
+$(OBJROOT)/%.rel: $(PRODUCT_DIR)/%.asm
+	@echo AS $<
+	@mkdir -p $(@D)
+	$(v)cp $< $(subst $(PRODUCT_DIR),$(OBJROOT),$<)
+	$(v)$(AS) $(ASFLAGS) $(subst $(PRODUCT_DIR),$(OBJROOT),$<)
+
+$(OBJROOT)/%.rel: $(LIB_DIR)/%.c
+	@mkdir -p $(@D)
+	@echo DEP $<
+	$(v)(/bin/echo -n $(@D)/ && $(CC) $(DEPFLAGS) $<) > $@.dep
+	@echo CC $<
+	$(v)$(CC) -c -o $@ $(CFLAGS) $<
+
+clean:
+	$(v)rm -rf $(OBJROOT)/../*
+
+#
+# Dependencies
+#
+GLOBAL_DEPS	+=	$(MAKEFILE_LIST)
+$(OBJS):	$(GLOBAL_DEPS)
+-include $(wildcard $(OBJROOT)/*.dep)
