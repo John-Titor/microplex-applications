b11312bb809371cb98b79ebd31a623cae9192df4
none <user@example.com>
2021-02-03 16:35:35 -0800

    Use a less obtuse way of referring to registers.

--- lib/mscan.c
+++ lib/mscan.c
@@ -3,6 +3,7 @@
  *
  */
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -32,53 +33,57 @@ CAN_init(CAN_bitrate bitrate,
         CAN_filter_mode filter_mode,
         const CAN_filters *filters)
 {
+    assert(bitrate <= (sizeof(btr_table) / sizeof(btr_table[0])));
+    assert(filter_mode <= CAN_FM_NONE);
 
     // set INITRQ and wait for it to be acknowledged
-    _CANCTL0.Byte = CANCTL0_INITRQ_MASK;
-    while (!(_CANCTL1.Byte & CANCTL1_INITAK_MASK)) {
+    CANCTL0 = CANCTL0_INITRQ_MASK;
+    while (!(CANCTL1 & CANCTL1_INITAK_MASK)) {
     }
 
     // enable MSCAN, select external clock 
-    _CANCTL1.Byte = CANCTL1_CANE_MASK;
+    CANCTL1 = CANCTL1_CANE_MASK;
 
     // configure for selected bitrate
-    _CANBTR0.Byte = btr_table[bitrate][0];
-    _CANBTR1.Byte = btr_table[bitrate][1];
+    CANBTR0 = btr_table[bitrate][0];
+    CANBTR1 = btr_table[bitrate][1];
 
     // configure filters
     if (filter_mode == CAN_FM_NONE) {
-        _CANIDAC.Byte = CANIDAC_IDAM1_MASK; // 8-bit filters
-        _CANIDAR0.Byte = 0;                 // mask result 0
-        _CANIDMR0.Byte = 0;                 // look at no bits
+        CANIDAC = CANIDAC_IDAM1_MASK; // 8-bit filters
+        CANIDAR0 = 0;                 // mask result 0
+        CANIDMR0 = 0;                 // look at no bits
     } else {
-        _CANIDAC.Byte = filter_mode << 4;
-        _CANIDAR0.Byte = filters->filter_8.accept[0];
-        _CANIDAR1.Byte = filters->filter_8.accept[1];
-        _CANIDAR2.Byte = filters->filter_8.accept[2];
-        _CANIDAR3.Byte = filters->filter_8.accept[3];
-        _CANIDAR4.Byte = filters->filter_8.accept[4];
-        _CANIDAR5.Byte = filters->filter_8.accept[5];
-        _CANIDAR6.Byte = filters->filter_8.accept[6];
-        _CANIDAR7.Byte = filters->filter_8.accept[7];
-        _CANIDMR0.Byte = filters->filter_8.mask[0];
-        _CANIDMR1.Byte = filters->filter_8.mask[1];
-        _CANIDMR2.Byte = filters->filter_8.mask[2];
-        _CANIDMR3.Byte = filters->filter_8.mask[3];
-        _CANIDMR4.Byte = filters->filter_8.mask[4];
-        _CANIDMR5.Byte = filters->filter_8.mask[5];
-        _CANIDMR6.Byte = filters->filter_8.mask[6];
-        _CANIDMR7.Byte = filters->filter_8.mask[7];
+        CANIDAC = filter_mode << 4;
+        CANIDAR0 = filters->filter_8.accept[0];
+        CANIDAR1 = filters->filter_8.accept[1];
+        CANIDAR2 = filters->filter_8.accept[2];
+        CANIDAR3 = filters->filter_8.accept[3];
+        CANIDAR4 = filters->filter_8.accept[4];
+        CANIDAR5 = filters->filter_8.accept[5];
+        CANIDAR6 = filters->filter_8.accept[6];
+        CANIDAR7 = filters->filter_8.accept[7];
+        CANIDMR0 = filters->filter_8.mask[0];
+        CANIDMR1 = filters->filter_8.mask[1];
+        CANIDMR2 = filters->filter_8.mask[2];
+        CANIDMR3 = filters->filter_8.mask[3];
+        CANIDMR4 = filters->filter_8.mask[4];
+        CANIDMR5 = filters->filter_8.mask[5];
+        CANIDMR6 = filters->filter_8.mask[6];
+        CANIDMR7 = filters->filter_8.mask[7];
     }
 
     // clear INITRQ and wait for it to be acknowledged
-    _CANCTL0.Byte &= ~CANCTL0_INITRQ_MASK;
-    while (_CANCTL1.Byte & CANCTL1_INITAK_MASK) {
+    CANCTL0 &= ~CANCTL0_INITRQ_MASK;
+    while (CANCTL1 & CANCTL1_INITAK_MASK) {
     }
 }
 
 bool
 CAN_send(const CAN_message *msg)
 {
+    assert(msg != NULL);
+
     // return false if not possible to send immediately.
     return _CAN_send(msg, WM_NONE);
 }
@@ -86,6 +91,8 @@ CAN_send(const CAN_message *msg)
 void
 CAN_send_blocking(const CAN_message *msg)
 {
+    assert(msg != NULL);
+
     // wait for space to send message
     _CAN_send(msg, WM_SPACE);
 }
@@ -93,6 +100,8 @@ CAN_send_blocking(const CAN_message *msg)
 void
 CAN_send_debug(const CAN_message *msg)
 {
+    assert(msg != NULL);
+
     // wait for space and wait for message to be sent -
     // debug messages are thus sent in the order they are
     // queued.
@@ -103,10 +112,13 @@ static bool
 _CAN_send(const CAN_message *msg,
           CAN_wait_mode wait_mode)
 {
+    assert(msg != NULL);
+    assert(wait_mode <= WM_SENT);
+
     uint8_t txe;
 
     // wait for a buffer to be free
-    while ((txe = _CANTFLG.Byte) == 0) {
+    while ((txe = CANTFLG) == 0) {
         // ... or don't
         if (wait_mode == WM_NONE) {
             return false;
@@ -114,32 +126,32 @@ _CAN_send(const CAN_message *msg,
     }        
 
     // select the buffer
-    _CANTBSEL.Byte = txe;
+    CANTBSEL = txe;
 
     // read back to work out which one we actually selected
-    txe = _CANTBSEL.Byte;
+    txe = CANTBSEL;
 
     // copy message to buffer
-    _CANTIDR0.Byte = msg->id.regs[0];
-    _CANTIDR1.Byte = msg->id.regs[1];
-    _CANTIDR2.Byte = msg->id.regs[2];
-    _CANTIDR3.Byte = msg->id.regs[3];
-    _CANTDSR0.Byte = msg->data[0];
-    _CANTDSR1.Byte = msg->data[1];
-    _CANTDSR2.Byte = msg->data[2];
-    _CANTDSR3.Byte = msg->data[3];
-    _CANTDSR4.Byte = msg->data[4];
-    _CANTDSR5.Byte = msg->data[5];
-    _CANTDSR6.Byte = msg->data[6];
-    _CANTDSR7.Byte = msg->data[7];
-    _CANTDLR.Byte = msg->dlc;
-    _CANTTBPR.Byte = msg->priority;
+    CANTIDR0 = msg->id.regs[0];
+    CANTIDR1 = msg->id.regs[1];
+    CANTIDR2 = msg->id.regs[2];
+    CANTIDR3 = msg->id.regs[3];
+    CANTDSR0 = msg->data[0];
+    CANTDSR1 = msg->data[1];
+    CANTDSR2 = msg->data[2];
+    CANTDSR3 = msg->data[3];
+    CANTDSR4 = msg->data[4];
+    CANTDSR5 = msg->data[5];
+    CANTDSR6 = msg->data[6];
+    CANTDSR7 = msg->data[7];
+    CANTDLR = msg->dlc;
+    CANTTBPR = msg->priority;
 
     // mark the buffer as not-empty to start transmission
-    _CANTFLG.Byte = txe;
+    CANTFLG = txe;
 
     // wait for message to send, or don't
-    while ((wait_mode == WM_SENT) && !(_CANTFLG.Byte & txe)) {
+    while ((wait_mode == WM_SENT) && !(CANTFLG & txe)) {
     }
     return true;
 }
@@ -147,35 +159,10 @@ _CAN_send(const CAN_message *msg,
 bool
 CAN_recv(CAN_message *msg)
 {
+    assert(msg != NULL);
+
     (void)msg;
     return false;
-}
-
-void
-CAN_puts(const char *str)
-{
-    CAN_message msg = {
-        .id = { .mscan_id = MSCAN_ID_EXT(0x1ffffffe) },
-        .priority = 0
-    };
-
-    for (;;) {
-        msg.dlc = 0;
-        for (;;) {
-            msg.data[msg.dlc++] = *str;
-            if ((msg.dlc == 8) || (*str == '\0')) {
-                while (!CAN_send(&msg)) {
-                    // spin until it can be sent
-                }
-                msg.priority++;
-                break;
-            }
-            str++;
-        }
-        if (*str == '\0') {
-            break;
-        }
-    }
 }
 
 int
