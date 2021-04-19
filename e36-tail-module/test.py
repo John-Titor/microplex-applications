0c3debdf2fafebe20c842f61f89d4272b852ea53
none <user@example.com>
2021-04-18 22:53:13 -0700

    more test functionality

--- e36-tail-module/test.py
+++ e36-tail-module/test.py
@@ -29,6 +29,18 @@ CYAN = 3
 ACK_ID = 0x1ffffff0
 CONSOLE_ID = 0x1ffffffe
 MJS_POWER_ID = 0x0fffffff
+LIGHT_CTRL_ID = 0x21a
+BRAKE_CTRL_ID = 0x0a8
+
+
+class MessageError(Exception):
+    """a received message was not as expected"""
+    pass
+
+
+class ModuleError(Exception):
+    """the module did something unexpected"""
+    pass
 
 
 class TXMessage(can.Message):
@@ -59,6 +71,33 @@ class MSG_mjs_power(TXMessage):
         super().__init__(MJS_POWER_ID, arg)
 
 
+class MSG_brake(TXMessage):
+    """BMW brake (etc.) status message"""
+    _format = 'BHHBBB'
+
+    def __init__(self, brake_state):
+        super().__init__(BRAKE_CTRL_ID,
+                         0x54,              # magic
+                         0,                 # actual torque
+                         0,                 # rounded torque
+                         240,               # clutch not depressed
+                         0x0f,              # magic
+                         32 if brake_state else 3)
+
+
+class MSG_lights(TXMessage):
+    """BMW light control message"""
+    _format = 'BBB'
+
+    def __init__(self, brake_light, tail_light, rain_light):
+        super().__init__(LIGHT_CTRL_ID,
+                         ((0x80 if brake_light else 0) |
+                          (0x04 if tail_light else 0) |
+                          (0x01 if rain_light else 0)),
+                         0,
+                         0xf7)
+
+
 class RXMessage(object):
     """
     Abstract for messages that have been received.
@@ -71,7 +110,8 @@ class RXMessage(object):
         if raw.arbitration_id != expected_id:
             raise MessageError(f'expected reply with ID 0x{expected_id:x} '
                                f'but got {raw}')
-        if raw.dlc != struct.calcsize(self._format):
+        expected_dlc = struct.calcsize(self._format)
+        if raw.dlc != expected_dlc:
             raise MessageError(f'expected reply with length {expected_dlc} '
                                f'but got {raw}')
 
@@ -191,14 +231,8 @@ class CANInterface(object):
                                       bitrate=args.can_speed,
                                       sleep_after_open=0.2)
 
-        # filter just the IDs we expect to see coming from the module
-        # self._bus.set_filters([
-        #     {"can_id": 0x1ffffff0,  "can_mask": 0x1ffffff0, "extended": True}
-        # ])
-
     def send(self, message):
         """send the message"""
-        log(f'CAN TX: {message}')
         self._bus.send(message, 1)
 
     def recv(self, timeout=2):
@@ -212,10 +246,7 @@ class CANInterface(object):
         while time.time() < deadline:
             wait_time = deadline - time.time()
             try:
-                msg = self._bus.recv(wait_time)
-                if msg is not None:
-                    log(f'CAN RX: {msg}')
-                return msg
+                return self._bus.recv(wait_time)
             except Exception:
                 pass
         return None
@@ -254,18 +285,20 @@ class CANInterface(object):
 
 class ModuleState(object):
 
-    def __init__(self):
+    def __init__(self, win):
+        self._win = win
         self._can_in_timeout = False
         self._can_did_timeout = False
-        self._module_reset_counter = 0
-        self._reset()        
+        self.module_resets = 0
+        self.message_errors = 0
+        self._reset()
 
     def _reset(self):
         self.status_system = None
         self.status_v_i = None
         self.status_faults = None
 
-    def update(msg):
+    def update(self, msg):
         try:
             self.status_system = MSG_status_system(msg)
         except MessageError:
@@ -275,9 +308,8 @@ class ModuleState(object):
                 try:
                     self.status_faults = MSG_status_faults(msg)
                 except MessageError:
-                    return False
+                    self.message_errors += 1
         self._can_in_timeout = False
-        return true
 
     def timeout(self):
         self._can_in_timeout = True
@@ -285,7 +317,7 @@ class ModuleState(object):
         self._reset()
 
     def module_reset(self):
-        self._module_reset_counter += 1
+        self.module_resets += 1
 
     def _text_attr(self, text, val, index):
         if val & (0x01 << index):
@@ -294,62 +326,86 @@ class ModuleState(object):
             return (text, curses.color_pair(CYAN))
         return ('-' * len(text), curses.A_DIM)
 
-    def print_attr(self, win, y, x, name, len=8, index=None):
-        for msg in [self.status_system, self.status_v_i, self.status_faults]:
+    def print_attr(self, y, x, attr, len=8, index=None):
+        for obj in [self, self.status_system, self.status_v_i, self.status_faults]:
             val = None
             try:
-                val = msg.getattr(attr)
+                val = getattr(obj, attr)
                 break
-            except AttributeError:
+            except AttributeError as e:
                 pass
         if val is None:
-            win.addstr(y, x, '-' * len, curses.A_DIM)
+            self._win.addstr(y, x, 'X', curses.color_pair(RED))
             return
 
-        if attr in ['t30_voltage', 't15_voltage']:
-            win.addstr(y, x, f' {val / 1000.0:{len}.2f}V', curses.A_BOLD)
+        elif attr in ['t30_voltage', 't15_voltage']:
+            self._win.addstr(y, x, f' {val / 1000.0:2.3f}', curses.A_BOLD)
 
-        if attr == 'temperature':
-            win.addstr(y, x, f'{val:{len}}°C', curses.A_BOLD)
+        elif attr == 'temperature':
+            self._win.addstr(y, x, f'{val:3}', curses.A_BOLD)
 
-        if attr == 'fuel_level':
-            win.addstr(y, x, f'{val:{len}}%', curses.A_BOLD)
+        elif attr == 'fuel_level':
+            self._win.addstr(y, x, f'{val:3}', curses.A_BOLD)
 
-        if attr == 'output_request':
+        elif attr == 'output_request':
             if val & (1 << index):
-                win.addstr(y, x, 'ON ', curses.color_pair(GREEN))
+                self._win.addstr(y, x, 'ON ', curses.color_pair(GREEN))
             else:
-                win.addstr(y, x, 'OFF', curses.A_BOLD)
+                self._win.addstr(y, x, 'OFF', curses.A_BOLD)
 
-        if attr == 'output_voltage':
-            win.addstr(y, x, f'{val[index] / 1000.0:{len}.3f}V', curses.A_BOLD)
+        elif attr == 'output_voltage':
+            self._win.addstr(y, x, f'{val[index] / 1000.0:2.3f}', curses.A_BOLD)
 
-        if attr == 'output_current':
-            win.addstr(y, x, f'{val[index] / 1000.0:{len}.3f}A', curses.A_BOLD)
+        elif attr == 'output_current':
+            self._win.addstr(y, x, f'{val[index] / 1000.0:2.3f}', curses.A_BOLD)
 
-        if attr == 'output_faults':
-            win.addstr(y, x, *self._text_attr('OPEN', val[index], index))
-            win.addstr(y, x + 5, *self._text_attr('STUCK', val[index], index))
-            win.addstr(y, x + 11, *self._text_attr('OVERLOAD', val[index], index))
+        elif attr == 'output_faults':
+            self._win.addstr(y, x, *self._text_attr('OPEN', val[index], index))
+            self._win.addstr(y, x + 5, *self._text_attr('STUCK', val[index], index))
+            self._win.addstr(y, x + 11, *self._text_attr('OVERLOAD', val[index], index))
 
-        if attr == 'system_faults':
-            stdscdr.addstr(y, x, *self._text_attr('T30', val, 0))
-            stdscdr.addstr(y, x + 4, *self._text_attr('T15', val, 1))
-            stdscdr.addstr(y, x + 8, *self._text_attr('CAN', val, 2))
-            stdscdr.addstr(y, x + 12, *self._text_attr('TEMP', val, 3))
+        elif attr == 'system_faults':
+            self._win.addstr(y, x, *self._text_attr('T30', val, 0))
+            self._win.addstr(y, x + 4, *self._text_attr('T15', val, 1))
+            self._win.addstr(y, x + 8, *self._text_attr('CAN', val, 2))
+            self._win.addstr(y, x + 12, *self._text_attr('TEMP', val, 3))
 
-        if attr == 'module_faults':
+        elif attr == 'module_faults':
             if self._can_in_timeout:
-                win.addstr(y, x, 'CAN', curses.color_pair(RED))
+                self._win.addstr(y, x, 'CAN', curses.color_pair(RED))
             if self._can_did_timeout:
-                win.addstr(y, x, 'CAN', curses.color_pair(CYAN))
-            win.addstr(y, x, '---', curses.A_DIM)
+                self._win.addstr(y, x, 'CAN', curses.color_pair(CYAN))
+            self._win.addstr(y, x, '---', curses.A_DIM)
 
-        if attr == 'module_resets':
-            color = curses.A_DIM if self._module_reset_counter == 0 else curses.color_pair(RED)
-            win.addstr(y, x, f'{self._module_reset_counter:{len}}', color)
+        elif attr in ['module_resets', 'message_errors']:
+            color = curses.A_DIM if val == 0 else curses.color_pair(RED)
+            self._win.addstr(y, x, f'{val:{len}}', color)
 
-        raise RuntimeError(f'cannot present \'{attr}\'')
+        else:
+            raise RuntimeError(f'cannot present \'{attr}\'')
+
+
+class Logger(object):
+    def __init__(self, win, args):
+        self._win = win
+        self._verbose = args.verbose
+        self._win.addstr(0, 0, "initializing...\n")
+        self._win.idlok(True)
+        self._win.scrollok(True)
+        self._win.refresh()
+        self._cons_buf = ""
+
+    def log_can(self, msg):
+        if msg.arbitration_id == CONSOLE_ID:
+            for idx in range(0, msg.dlc):
+                if msg.data[idx] == 0:
+                    self._win.addstr(f"CONS: {self._cons_buf}\n")
+                    self._cons_buf = ""
+                else:
+                    self._cons_buf += chr(msg.data[idx])
+        elif self._verbose:
+            self._win.addstr(f"CAN: {msg}\n")
+        self._win.refresh()
 
 
 # Live monitoring mode for testing, etc.
@@ -377,71 +433,83 @@ def do_monitor(stdscr, interface, args):
 
     module_id = interface.detect()
 
-    state = ModuleState()
 
     sw_t15 = False
     sw_brake = False
     sw_lights = False
     sw_rain = False
     sw_can = False
+    tx_toggle = False
 
     curses.curs_set(False)
     curses.start_color()
     stdscr.nodelay(True)
     stdscr.clear()
+    stdscr.refresh()
 
     curses.init_pair(RED, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(GREEN, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(CYAN, curses.COLOR_CYAN, curses.COLOR_BLACK)
 
-    stdscr.addstr(1, 1, '-- Monitor Status -------------------------------------')
-    stdscr.addstr(2, 1, 'Resets: ')
-    stdscr.addstr(3, 1, 'Faults: ')
+    statwin = curses.newwin(18, 80, 0, 0)
+    state = ModuleState(statwin)
 
-    stdscr.addstr(5, 1, '-- Module Status --------------------------------------')
-    stdscr.addstr(6, 1, 'T30 [        ]  T15 [        ]  Temp [        ]')
-    stdscr.addstr(7, 1, 'Faults:')
+    statwin.addstr(1, 1, '-- Monitor Status -------------------------------------')
+    statwin.addstr(2, 1, 'Resets:           Message Errors:')
+    statwin.addstr(3, 1, 'Faults:')
 
-    stdscr.addstr(9, 1, 'Output    Status  Voltage     Current     Faults')
-    stdscr.addstr(10, 4, '1')
-    stdscr.addstr(11, 4, '2')
-    stdscr.addstr(12, 4, '3')
-    stdscr.addstr(13, 4, '4')
+    statwin.addstr(5, 1, '-- Module Status --------------------------------------')
+    statwin.addstr(6, 1, 'T30      V  T15      V  Temp    °C')
+    statwin.addstr(7, 1, 'Faults:')
 
-    stdscr.addstr(15, 1, '[T]15      [B]rake      [L]ights      [R]ain      [C]AN')
-    stdscr.addstr(16, 1, '[Q]uit')
-    stdscr.refresh()
+    statwin.addstr(9, 1, 'Output    Status  Voltage     Current     Faults')
+    statwin.addstr(10, 4, '1                   V           A')
+    statwin.addstr(11, 4, '2                   V           A')
+    statwin.addstr(12, 4, '3                   V           A')
+    statwin.addstr(13, 4, '4                   V           A')
+
+    statwin.addstr(15, 1, '[T]15      [B]rake      [L]ights      [R]ain      [C]AN')
+    statwin.addstr(16, 1, '[Q]uit')
+    statwin.refresh()
+
+    maxy, maxx = stdscr.getmaxyx()
+    logwin = curses.newwin(maxy - 18, maxx, 18, 0)
+    logger = Logger(logwin, args)
 
     def status_flag(x, flag):
         if flag:
-            stdscr.addstr(15, x, 'ON ', curses.color_pair(GREEN))
+            statwin.addstr(15, x, 'ON ', curses.color_pair(GREEN))
         else:
-            stdscr.addstr(15, x, 'OFF', curses.A_BOLD)
+            statwin.addstr(15, x, 'OFF', curses.A_BOLD)
 
     last_send_time = 0
 
     spindex = 0
     while True:
-        #msg = interface.recv(1)
-        msg = None
+        statwin.refresh()
+        msg = interface.recv(0.2)
         if msg is None:
-            state.timeout()
+            #state.timeout()
+            pass
         else:
+            logger.log_can(msg)
             # XXX handle module-reset message?
             state.update(msg)
+            spindex = 0 if spindex == 3 else spindex + 1
 
-        state.print_attr(stdscr, 2, 9, 'module_resets')
-        state.print_attr(stdscr, 3, 9, 'module_faults')
+        state.print_attr(2, 9, 'module_resets')
+        state.print_attr(2, 35, 'message_errors')
+        state.print_attr(3, 9, 'module_faults')
 
-        state.print_attr(stdscr, 6, 6, 't30_voltage')
-        state.print_attr(stdscr, 6, 22, 't15_voltage')
-        state.print_attr(stdscr, 6, 39, 't30_voltage')
+        state.print_attr(6, 4, 't30_voltage')
+        state.print_attr(6, 16, 't15_voltage')
+        state.print_attr(6, 30, 'temperature')
 
         for channel in range(0, 4):
-            state.print_attr(stdscr, 10 + channel, 12, 'output_request', len=3, index=channel)
-            state.print_attr(stdscr, 10 + channel, 19, 'output_voltage', index=channel)
-            state.print_attr(stdscr, 10 + channel, 31, 'output_current', index=channel)
-            state.print_attr(stdscr, 10 + channel, 43, 'output_faults', len=19, index=channel)
+            state.print_attr(10 + channel, 12, 'output_request', len=3, index=channel)
+            state.print_attr(10 + channel, 19, 'output_voltage', index=channel)
+            state.print_attr(10 + channel, 31, 'output_current', index=channel)
+            state.print_attr(10 + channel, 43, 'output_faults', len=19, index=channel)
 
         status_flag(7, sw_t15)
         status_flag(20, sw_brake)
@@ -449,13 +517,26 @@ def do_monitor(stdscr, interface, args):
         status_flag(46, sw_rain)
         status_flag(57, sw_can)
 
-        stdscr.addstr(1, 1, '/-\\-'[spindex])
-        spindex = 0 if spindex == 3 else spindex + 1
+        statwin.addstr(1, 1, '/-\\-'[spindex])
+
+        if sw_can:
+            if tx_toggle:
+                msg = MSG_brake(sw_brake)
+            else:
+                msg = MSG_lights(False, sw_lights, sw_rain)
+                # msg = MSG_lights(sw_brake, sw_lights, sw_rain)
+            logger.log_can(msg)
+            interface.send(msg)
+            tx_toggle = not tx_toggle
 
         try:
             ch = stdscr.getkey()
             if ch == 't' or ch == 'T':
                 sw_t15 = not sw_t15
+                if sw_t15:
+                    interface.set_power_t30_t15()
+                else:
+                    interface.set_power_t30()
             if ch == 'b' or ch == 'B':
                 sw_brake = not sw_brake
             if ch == 'l' or ch == 'L':
@@ -495,15 +576,11 @@ actiongroup.add_argument('--monitor',
                          help='monitor operation')
 
 args = parser.parse_args()
-if args.verbose:
-    def log(msg):
-        print(msg)
-else:
-    def log(msg):
-        pass
 try:
     interface = CANInterface(args)
     if args.monitor:
         curses.wrapper(do_monitor, interface, args)
 except KeyboardInterrupt:
     pass
+if interface is not None:
+    interface.set_power_off()
