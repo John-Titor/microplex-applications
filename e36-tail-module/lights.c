0c9f8cf88140f8dd22228d6b7b2d81a6f994d3a1
none <user@example.com>
2021-04-18 22:55:50 -0700

    improve lights logic

--- e36-tail-module/lights.c
+++ e36-tail-module/lights.c
@@ -14,9 +14,21 @@ static light_state_t rain_light_requested;
 void
 brake_light_request(light_state_t state)
 {
-    brake_light_requested = state;
-    if (state == LIGHT_OFF) {
-        pt_reset(&pt_brakes);
+    // Requests are repeated regularly with the current
+    // state; only do something if we see a different
+    // state requested.
+    //
+    if (state != brake_light_requested) {
+
+        // If turning off, also reset the thread.
+        //
+        if (state == LIGHT_OFF) {
+            pt_reset(&pt_brakes);
+        }
+
+        // Update new requested state.
+        //
+        brake_light_requested = state;
     }
 }
 
@@ -24,55 +36,90 @@ void
 brake_thread(struct pt *pt)
 {
     static timer_t brake_timer = { .delay_ms = 0 };
+    static timer_t brake_reset_timer = { .delay_ms = 0 };
 
     pt_begin(pt);
+
+    // Lazy timer registration.
+    //
     if (!timer_registered(brake_timer)) {
         timer_register(&brake_timer);
+        timer_register(&brake_reset_timer);
     }
 
+    // Turn lights off
+    //
     output_request(OUTPUT_BRAKE_L, false);
     output_request(OUTPUT_BRAKE_R, false);
 
+    // Wait for light to be turned on.
+    //
+    // Set a timer to detect whether we've been off for
+    // more than 4s.
+    //
+    timer_reset(brake_reset_timer, 4000);
     pt_wait(pt, brake_light_requested != LIGHT_OFF);
 
-    output_request(OUTPUT_BRAKE_L, true);
-    output_request(OUTPUT_BRAKE_R, true);
-    pt_delay(pt, brake_timer, 200);
+    // If we have been off for > 4s, do the brake-on
+    // animation.
+    //
+    if (!timer_expired(brake_reset_timer)) {
 
-    output_request(OUTPUT_BRAKE_L, false);
-    output_request(OUTPUT_BRAKE_R, true);
-    pt_delay(pt, brake_timer, 200);
+        output_request(OUTPUT_BRAKE_L, true);
+        output_request(OUTPUT_BRAKE_R, true);
+        pt_delay(pt, brake_timer, 200);
 
-    output_request(OUTPUT_BRAKE_L, true);
-    output_request(OUTPUT_BRAKE_R, false);
-    pt_delay(pt, brake_timer, 200);
+        output_request(OUTPUT_BRAKE_L, false);
+        output_request(OUTPUT_BRAKE_R, true);
+        pt_delay(pt, brake_timer, 200);
 
-    output_request(OUTPUT_BRAKE_L, false);
-    output_request(OUTPUT_BRAKE_R, true);
-    pt_delay(pt, brake_timer, 200);
+        output_request(OUTPUT_BRAKE_L, true);
+        output_request(OUTPUT_BRAKE_R, false);
+        pt_delay(pt, brake_timer, 200);
 
-    output_request(OUTPUT_BRAKE_L, true);
-    output_request(OUTPUT_BRAKE_R, false);
-    pt_delay(pt, brake_timer, 200);
+        output_request(OUTPUT_BRAKE_L, false);
+        output_request(OUTPUT_BRAKE_R, true);
+        pt_delay(pt, brake_timer, 200);
 
+        output_request(OUTPUT_BRAKE_L, true);
+        output_request(OUTPUT_BRAKE_R, false);
+        pt_delay(pt, brake_timer, 200);
+    }
+
+    // Lights stay on now until thread is reset.
+    //
     output_request(OUTPUT_BRAKE_L, true);
     output_request(OUTPUT_BRAKE_R, true);
 
     pt_end(pt);
 }
 
+
 void
 tail_light_request(light_state_t state)
 {
-    tail_light_requested = state;
-    pt_reset(&pt_tails);
+    // If we are not in rain light mode, and this is a change
+    // of state for the tail lights, update the requested state
+    // and reset the thread to make it so.
+    //
+    if ((rain_light_requested != LIGHT_ON) &&
+        (tail_light_requested != state)) {
+
+        tail_light_requested = state;
+        pt_reset(&pt_tails);
+    }
 }
 
 void
 rain_light_request(light_state_t state)
 {
-    rain_light_requested = state;
-    pt_reset(&pt_tails);
+    // If this is a change of state for the rain lights, update
+    // the requested state and reset the thread to make it so.
+    //
+    if (rain_light_requested != state) {
+        rain_light_requested = state;
+        pt_reset(&pt_tails);
+    }
 }
 
 void
@@ -81,10 +128,15 @@ tails_thread(struct pt *pt)
     static timer_t tails_timer = { .delay_ms = 0 };
 
     pt_begin(pt);
+ 
+    // Lazy timer registration.
+    //
     if (!timer_registered(tails_timer)) {
         timer_register(&tails_timer);
     }
 
+    // If rain light requested, blink lights at 2Hz.
+    //
     while (rain_light_requested == LIGHT_ON) {
         output_request(OUTPUT_TAILS, OUTPUT_STATE_ON);
         pt_delay(pt, tails_timer, 250);
@@ -92,6 +144,8 @@ tails_thread(struct pt *pt)
         pt_delay(pt, tails_timer, 250);
     }
 
+    // Set tail lights on/off as requested.
+    //
     if (tail_light_requested == LIGHT_ON) {
         output_request(OUTPUT_TAILS, OUTPUT_STATE_ON);
     } else {
